5cfcf100d06668e9b6f51a836c195bf71c1a0595
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-02-05 14:30:41 +0300

    Implement subset checking using Counter

---
+++ Array Subset.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def isSubset(self, a, b):
+        freq_a = Counter(a)
+        freq_b = Counter(b)
+        
+        for key in freq_b: 
+            if freq_b[key] > freq_a.get(key, 0): 
+                return False 
+        
+        return True
