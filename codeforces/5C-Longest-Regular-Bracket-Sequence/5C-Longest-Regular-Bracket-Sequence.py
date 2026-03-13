c5d51b857b304ef16ceade247c3653d98a635685
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-03-13 16:16:14 +0300

    Add solution for Longest Regular Bracket Sequence

---
+++ codeforces/5C-Longest-Regular-Bracket-Sequence/5C-Longest-Regular-Bracket-Sequence.py
@@ -0,0 +1,25 @@
+s = input()
+
+stack = [-1]
+max_len = 0
+count = 0
+
+for i, ch in enumerate(s):
+    if ch == '(':
+        stack.append(i)
+    else:
+        stack.pop()
+        if not stack:
+            stack.append(i)
+        else:
+            longest = i - stack[-1]
+            if longest > max_len:
+                max_len = longest
+                count = 1
+            elif longest == max_len:
+                count += 1
+
+if max_len == 0:
+    print(0, 1)
+else:
+    print(max_len, count)
