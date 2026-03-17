7a85318a7d383bdc8b3393cd401163ca29bc83a1
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-03-17 17:19:22 +0300

    Add solution for Remove Nodes From Linked List

--- leetcode/2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
+++ leetcode/2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
@@ -4,18 +4,33 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        stack = []
-        curr = head
-
-        while curr:
-            while stack and stack[-1].val < curr.val:
-                stack.pop()
-            stack.append(curr)
-            curr = curr.next
+    def removeNodes(self, head: ListNode) -> ListNode:
+        def helper(node: ListNode) -> ListNode:
+            if not node:
+                return None
+            
+            node.next = helper(node.next)
+            
+            if node.next and node.val < node.next.val:
+                return node.next
+            else:
+                return node
         
-        for i in range(len(stack) - 1):
-            stack[i].next = stack[i + 1]
-        stack[-1].next = None
+        return helper(head)
 
-        return stack[0]
+        #Monotonic stack
+        # stack = []
+        # curr = head
+
+        # while curr:
+        #     while stack and stack[-1].val < curr.val:
+        #         stack.pop()
+        #     stack.append(curr)
+        #     curr = curr.next
+
+        # for i in range(len(stack) - 1):
+        #     stack[i].next = stack[i + 1]
+        # stack[-1].next = None
+
+        # return stack[0]
+
