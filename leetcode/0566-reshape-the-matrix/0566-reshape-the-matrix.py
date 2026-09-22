068072d00a8cc3c922a8f49a4d56ba967eb975bd
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-09-22 17:55:04 +0300

    Add solution for Reshape the Matrix

---
+++ leetcode/0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -0,0 +1,7 @@
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        if m * n != r * c:
+            return mat
+        flat = [num for row in mat for num in row]
+        return [flat[i*c:(i+1)*c] for i in range(r)]
